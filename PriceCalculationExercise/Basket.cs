6ba1cd8dc08e2c3f290747d2b42a7d7d94992d33
minkaotic <mia.filisch@7digital.com>
2018-11-11 08:55:26 +0000

    Return total of 0 when basket is empty

---
+++ PriceCalculationExercise/Basket.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PriceCalculationExercise
+{
+    public class Basket
+    {
+        public double Total()
+        {
+            return 0;
+        }
+    }
+}
---
+++ PriceCalculationTests/BasketTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using PriceCalculationExercise;
+
+namespace PriceCalculationTests
+{
+    public class BasketTests
+    {
+        [Test]
+        public void Given_the_basket_is_empty_the_total_should_be_zero()
+        {
+            var basket = new Basket();
+
+            var total = basket.Total();
+
+            Assert.That(total, Is.EqualTo(0));
+        }
+    }
+}
