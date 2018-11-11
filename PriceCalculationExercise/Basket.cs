a578925ac4e636f96b9bace4880353e8a7adae7f
minkaotic <mia.filisch@7digital.com>
2018-11-11 09:03:11 +0000

    Return correct total for 1 butter

--- PriceCalculationExercise/Basket.cs
+++ PriceCalculationExercise/Basket.cs
@@ -1,16 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace PriceCalculationExercise
 {
     public class Basket
     {
+        private List<string> _contents = new List<string>();
+
         public double Total()
         {
+            if (_contents.Contains("butter"))
+            {
+                return 0.80;
+            }
             return 0;
+        }
+
+        public void Add(string butter)
+        {
+            _contents.Add("butter");
         }
     }
 }
--- PriceCalculationTests/BasketTests.cs
+++ PriceCalculationTests/BasketTests.cs
@@ -14,5 +14,16 @@ namespace PriceCalculationTests
 
             Assert.That(total, Is.EqualTo(0));
         }
+
+        [Test]
+        public void Given_the_basket_contains_one_butter_the_total_should_be_80_pence()
+        {
+            var basket = new Basket();
+            basket.Add("butter");
+
+            var total = basket.Total();
+
+            Assert.That(total, Is.EqualTo(0.80));
+        }
     }
 }
