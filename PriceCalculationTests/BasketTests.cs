9a0dd0b01e88033a23ab1b93a9557f0502da75fb
minkaotic <mia.filisch@7digital.com>
2018-11-11 14:15:00 +0000

    Refactor BasketTests to extract repeated basket creation into SetUp

--- PriceCalculationTests/BasketTests.cs
+++ PriceCalculationTests/BasketTests.cs
@@ -5,12 +5,18 @@ namespace PriceCalculationTests
 {
     public class BasketTests
     {
-        [Test]
-        public void Given_the_basket_is_empty_the_total_should_be_zero()
-        {
-            var basket = new Basket();
+        private Basket _basket;
 
-            var total = basket.Total();
+        [SetUp]
+        public void SetUp()
+        {
+            _basket = new Basket();
+        }
+
+        [Test]
+        public void Given_the__basket_is_empty_the_total_should_be_zero()
+        {
+            var total = _basket.Total();
 
             Assert.That(total, Is.EqualTo(0));
         }
@@ -18,74 +24,69 @@ namespace PriceCalculationTests
         [TestCase("butter", 0.80)]
         [TestCase("milk", 1.15)]
         [TestCase("bread", 1.00)]
-        public void Given_the_basket_contains_only_one_item_the_total_should_be_indvidual_item_price(string item, double expectedPrice)
+        public void Given_the__basket_contains_only_one_item_the_total_should_be_individual_item_price(string item, double expectedPrice)
         {
-            var basket = new Basket();
-            basket.Add(item);
+            _basket.Add(item);
 
-            var total = basket.Total();
+            var total = _basket.Total();
 
             Assert.That(total, Is.EqualTo(expectedPrice));
         }
 
         [Test]
-        public void Given_the_basket_has_1_bread_1_butter_and_1_milk_the_total_should_be_2_95()
+        public void Given_the__basket_has_1_bread_1_butter_and_1_milk_the_total_should_be_2_95()
         {
-            var basket = new Basket();
-            basket.Add("bread");
-            basket.Add("butter");
-            basket.Add("milk");
+            _basket.Add("bread");
+            _basket.Add("butter");
+            _basket.Add("milk");
 
-            var total = basket.Total();
+            var total = _basket.Total();
 
             Assert.That(total, Is.EqualTo(2.95));
         }
 
         [Test]
-        public void Given_the_basket_has_2_butter_and_2_bread_the_total_should_be_3_10()
+        public void Given_the__basket_has_2_butter_and_2_bread_the_total_should_be_3_10()
         {
-            var basket = new Basket();
-            basket.Add("butter");
-            basket.Add("butter");
-            basket.Add("bread");
-            basket.Add("bread");
+            _basket.Add("butter");
+            _basket.Add("butter");
+            _basket.Add("bread");
+            _basket.Add("bread");
 
-            var total = basket.Total();
+            var total = _basket.Total();
 
             Assert.That(total, Is.EqualTo(3.10));
         }
 
         [Test]
-        public void Given_the_basket_has_4_milk_the_total_should_be_3_45()
+        public void Given_the__basket_has_4_milk_the_total_should_be_3_45()
         {
-            var basket = new Basket();
-            basket.Add("milk");
-            basket.Add("milk");
-            basket.Add("milk");
-            basket.Add("milk");
+            _basket.Add("milk");
+            _basket.Add("milk");
+            _basket.Add("milk");
+            _basket.Add("milk");
 
-            var total = basket.Total();
+            var total = _basket.Total();
 
             Assert.That(total, Is.EqualTo(3.45));
         }
 
         [Test]
-        public void Given_the_basket_has_2_butter_1_bread_and_8_milk_the_total_should_be_9_00()
+        public void Given_the__basket_has_2_butter_1_bread_and_8_milk_the_total_should_be_9_00()
         {
-            var basket = new Basket();
-            basket.Add("butter");
-            basket.Add("butter");
-            basket.Add("bread");
-            basket.Add("milk");
-            basket.Add("milk");
-            basket.Add("milk");
-            basket.Add("milk");
-            basket.Add("milk");
-            basket.Add("milk");
-            basket.Add("milk");
-            basket.Add("milk");
+            _basket.Add("butter");
+            _basket.Add("butter");
+            _basket.Add("bread");
+            _basket.Add("milk");
+            _basket.Add("milk");
+            _basket.Add("milk");
+            _basket.Add("milk");
+            _basket.Add("milk");
+            _basket.Add("milk");
+            _basket.Add("milk");
+            _basket.Add("milk");
 
-            var total = basket.Total();
+            var total = _basket.Total();
 
             Assert.That(total, Is.EqualTo(9.00));
         }
